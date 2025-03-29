88b16c9f413048a730ac6446350637d0821ba5f0
Yasin Rezaei <52451497+YasinRezaee@users.noreply.github.com>
2025-03-29 14:34:53 +0330

    reactive forms and form array

--- src/app/practice-field/practice-field.component.ts
+++ src/app/practice-field/practice-field.component.ts
@@ -1,20 +1,54 @@
 import { Component, OnInit } from '@angular/core';
-import { FormsModule, NgForm } from '@angular/forms';
+import { FormArray, FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatIcon } from '@angular/material/icon';
 
 @Component({
   selector: 'app-practice-field',
-  imports: [FormsModule],
+  imports: [FormsModule, ReactiveFormsModule],
   templateUrl: './practice-field.component.html',
   styleUrl: './practice-field.component.css'
 })
 export class PracticeFieldComponent implements OnInit {
+  genderList = ['Male', 'Female'];
+  signUpForm!: FormGroup;
 
-  constructor() { }
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
-  } 
+    this.formListSighUp();
+  }
 
-  onSubmit(form: NgForm){
-    console.log("Submitted" + form.value);
+  formListSighUp() {
+    this.signUpForm = this.fb.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      text: ['', Validators.required],
+      option: ['', Validators.required],
+      gender: ['', Validators.required],
+      address: this.fb.group({
+        street: ['', Validators.required],
+        city: ['', Validators.required],
+        zip: ['', Validators.required],
+      }),
+      hobbies: this.fb.array([])
+    });
+  }
+
+  get newHobbies(): FormArray {
+    return this.signUpForm.get('hobbies') as FormArray;
+  }
+
+  removeHobby(index: number) {
+    this.newHobbies.removeAt(index);
+  }
+
+  onAddHobbies() {
+    const hobbyControl = new FormControl('', Validators.required);
+    this.newHobbies.push(hobbyControl);
+  }
+
+  onsubmit(formValue: any) {
+    console.log(formValue);
+    // Handle form submission here
   }
 }
